a46285d3e7ebf6d32c6833f32a7e6ce808a77756
Richard Song <richardsong18@gmail.com>
2018-03-11 10:17:11 -0500

    add RESTful action for index

--- app/controllers/v1/contacts_controller.rb
+++ app/controllers/v1/contacts_controller.rb
@@ -1,22 +1,8 @@
 class V1::ContactsController < ApplicationController
   
-  def contact_info 
+  def index 
     contacts = Contact.all 
-
-    contacts_array = []
-    index = 0 
-
-    contacts.length.times do 
-      contact_hash = {}
-      contact_hash[:first_name] = contacts[index]["first_name"]
-      contact_hash[:last_name] = contacts[index]["last_name"]
-      contact_hash[:email] = contacts[index]["email"]
-      contact_hash[:phone_number] = contacts[index]["phone_number"]
-      contacts_array << contact_hash 
-      index += 1
-    end 
-
-    render json: {contacts: contacts_array} 
+    render json: contacts.as_json
 
   end 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   namespace :v1 do 
-    get "/contacts_site" => "contacts#contact_info"
+
+    get "/contacts" => "contacts#index"
+    
   end 
+
 end
