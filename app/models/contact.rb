bc3dfd67ebd9fbac118ee5cc0af1d0aa8a2402d9
Richard Song <richardsong18@gmail.com>
2018-03-12 19:33:41 -0500

    Add custom json response using model methods

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,28 @@
 class Contact < ApplicationRecord
+
+  def friendly_updated_at 
+    updated_at.strftime("%B %e, %Y")
+  end 
+
+  def full_name
+    full_name = first_name + " " + last_name
+  end 
+
+  def japanese_phone_number
+    japanese_phone_number = "+81" + phone_number
+  end 
+
+  def as_json 
+    { 
+      first_name: first_name,
+      last_name: last_name,
+      full_name: full_name,
+      email: email, 
+      phone_number: phone_number, 
+      japanese_phone_number: japanese_phone_number, 
+      updated_at: friendly_updated_at
+    }
+
+  end 
+  
 end
