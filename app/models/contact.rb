cfdc6bbef1cb1f658343057509c9074538c7e987
Richard Song <richardsong18@gmail.com>
2018-03-13 20:51:22 -0500

    add validations to contacts model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,9 @@
 class Contact < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true 
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  
 
   def friendly_updated_at 
     updated_at.strftime("%B %e, %Y")
@@ -14,6 +19,7 @@ class Contact < ApplicationRecord
 
   def as_json 
     { 
+      id: id, 
       first_name: first_name,
       middle_name: middle_name, 
       last_name: last_name,
